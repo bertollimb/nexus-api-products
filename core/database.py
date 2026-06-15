d991b1c5249bf53679ce4c706252bde22d9b3723
Matheus bertolli <matheusbertollianastacio@gmail.com>
2026-06-15 18:13:17 +0100

    feat: added database settings async

---
+++ core/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, AsyncSession
+from core.configs import settings
+
+Base = declarative_base()
+
+engine: AsyncEngine = create_async_engine(settings.DB_URL)
+
+Session: sessionmaker = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+    class_=AsyncSession,
+    bind=engine
+)
