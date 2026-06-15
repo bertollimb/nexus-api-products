972c84a0f26220ec98faa3264621f7ea7b8cb32b
Matheus bertolli <matheusbertollianastacio@gmail.com>
2026-06-15 18:19:06 +0100

    feat: adds application settings via pydantic-settings

---
+++ core/configs.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    API_V1_STR: str = '/api/v1'
+    DB_URL: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=True
+    )
+
+settings = Settings()
